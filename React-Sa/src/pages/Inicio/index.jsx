efb6f3a4093af94ad52e9f84743bfdbad4256d44
Maria Vitória Barizon Alves <maria_vb_alves@estudante.sesisenai.org.br>
2026-01-26 14:31:12 -0300

    SaMaria-26-01

---
+++ React-Sa/src/pages/Inicio/index.jsx
@@ -0,0 +1,44 @@
+import './style.css'
+
+function Inicio() {
+
+  return (
+
+    <> 
+      <header id='topo'>
+       {/* Conteúdo do header */}
+      </header>
+
+      <div id='recursos'>
+
+        <div id='esqR'>
+          <p>oiiii</p>
+        </div>
+
+        <div id='dirR'>
+          <p>oieee</p>
+        </div>
+
+      </div> 
+
+      <div id='quemSomos'>
+        <h1>oie</h1>
+      </div> 
+
+      <div id='Oqueoferecemos'>
+        <h1>oiI</h1>
+      </div> 
+
+
+
+
+    </>
+
+
+
+  )
+}
+
+
+
+export default Inicio
