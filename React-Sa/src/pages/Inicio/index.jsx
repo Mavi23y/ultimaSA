0154a48ecde83e025858707c9dd39048a8dc7557
Maria Vitória Barizon Alves <maria_vb_alves@estudante.sesisenai.org.br>
2026-01-28 17:06:15 -0300

    SaMaria-28-01

--- React-Sa/src/pages/Inicio/index.jsx
+++ React-Sa/src/pages/Inicio/index.jsx
@@ -1,33 +1,64 @@
 import './style.css'
+import fotoMedico from './img/medico.png';
+import fotoCalendario from './img/calendario.png';
 
 function Inicio() {
 
   return (
 
-    <> 
+    <>
+
+
+      {/* Conteúdo do header */}
       <header id='topo'>
-       {/* Conteúdo do header */}
       </header>
 
+      {/* Recursos */}
       <div id='recursos'>
 
         <div id='esqR'>
-          <p>oiiii</p>
+
+          <h1 id='tituloR'>
+            CONTROLE SEUS RECURSOS
+          </h1>
+
+            <p id='textoR'>
+              Pensando em suas necessidades, Oferecemos uma plataforma que permite uma melhor organização dos seus medicamentos.
+            </p>
+
+            <button id='btnR'>
+              Peça seu medicamento aqui!
+            </button>
+
         </div>
 
         <div id='dirR'>
-          <p>oieee</p>
+          <img id='foto-medico' src={fotoMedico} alt="Foto do Médico" />
         </div>
 
-      </div> 
+      </div>
 
-      <div id='quemSomos'>
-        <h1>oie</h1>
-      </div> 
+      {/* Fundo: Quem somos */}
+      <div id='baseF1'> </div>
+      <section className='fundo'></section>
+      <div id='baseF2'> </div>
 
+      {/* O que oferecemos */}
       <div id='Oqueoferecemos'>
-        <h1>oiI</h1>
-      </div> 
+
+        <div id='esqQ'>
+          <p>oiiii</p>
+        </div>
+
+        <div id='dirQ'>
+          
+          <img id='foto-calendario' src={fotoCalendario} alt="Foto do Calendário" />
+          <h1 id='tituloQ'>ORGANIZAÇÃO</h1>
+          <p id='textoQ'>Organizamos seus horários, entregas e o cuidado com seus remédios. Uma planilha completa e organizada com todas as informações necessárias para o cuidado de seus produtos.</p>
+
+        </div>
+
+      </div>
 
 
 
